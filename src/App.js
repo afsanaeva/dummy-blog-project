26e220ca8229026c67c5d4d5774c016d10dd6f01
afsanaeva <afsanaeva05@gmail.com>
2022-02-08 16:58:50 +0600

    login, register

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import Home from "./pages/home/Home";
-import TopBar from "./topbar/TopBar";
+import Single from "./pages/single/Single";
+import TopBar from "./components/topbar/TopBar";
+// import Homepage from "./pages/homepage/Homepage";
+// import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+// import Settings from "./pages/settings/Settings";
+// import Write from "./pages/write/Write";
 
 function App() {
   return (
     <>
       <TopBar />
-      <Home/>
+      <Register/>
     </>
   );
 }
